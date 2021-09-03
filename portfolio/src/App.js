9693c01b9351408fcb2f098b5e4f98ac16b7c6b6
jazperez <jazmin.perez.634@my.csun.edu>
2021-09-02 17:38:55 -0700

    Add create-react-app

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfolio in progress.<br></br>
+          Please come back later 😊 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
